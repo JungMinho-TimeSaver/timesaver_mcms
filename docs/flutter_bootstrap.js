29a654a2f6a2a58b8b5670ed8afc6a4b44931246
JungMinho-TimeSaver <jmh3606@gmail.com>
2025-08-09 22:43:32 +0900

    deploy: 2025-08-09 22:42

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1366991728"
+    serviceWorkerVersion: "515168948"
   }
 });
